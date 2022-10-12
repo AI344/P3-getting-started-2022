3e19db45038d44e7123cefa6388922538e97285b
Paul Griffiths <paul.griffiths@gmail.com>
2022-10-12 13:42:05 +0100

    Update plot_netcdf.py

--- plot_netcdf.py
+++ plot_netcdf.py
@@ -1,18 +1,28 @@
 # %% 
 import netCDF4 as nc
 import pylab as plt
+import numpy as np
 # %%
 data_to_plot=nc.Dataset('u-be647_ann_mean_surface_level_o3.nc')
 # %%
 data_to_plot.variables['o3']
 # %%
+# %% naive mean without surface area weighting
+meano3 = np.mean(data_to_plot.variables['o3'][:]*28.8/48*1e9*surface(lat,lon)/sum(surface), axis=(0,2,3))
 
-# plot mean O3 timeseries
+# %%
+# test code to calculate surface area weighting - AI to check and fix
+for ilat in lats:
+    for ilon in lons:
+        wtdo3 = o3[lat,lon]*surfacearea(lat,lon)
+        
+areawtmeano3 = np.sum(wtdo3)/np.sum(surfacearea)
 
-# plot surface area weighted mean O3
+###---### using xarray module
 
+# %%
 # plot O3 as a colormap
-plt.pcolormesh(data_to_plot.variables['o3'][0,0,:,:]*28.8/48*1e9, vmin=0, vmax=50)
+plt.pcolormesh(data_to_plot.variables['o3'][6,0,:,:]*28.8/48*1e9, vmin=0, vmax=50)
 plt.colorbar()
 # %%
 import cartopy.crs as ccrs
@@ -30,4 +40,5 @@ plt.pcolormesh(data_to_plot.variables['longitude'][:],
 ax.coastlines()
 #ax.stock_img()
 plt.show()
+#plt.colorbar()
 # %%
